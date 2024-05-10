19c4674ee624fdda6631fe31357fcc77f7c43302
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-05-10 19:14:28 +0200

    added long description

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='libml_URLPhishing',
     version='0.0.10',
+    long_description='Library to download and process data for the URL phishing project',
     description='Library to download and process data for the URL phishing project',
     author='Shantnav Agarwal',
     project_urls={
