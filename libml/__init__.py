b41568fcc5ccc835f14d59147c66e1e944ac1035
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-05-10 18:36:32 +0200

    Fixed init.py

--- libml/__init__.py
+++ libml/__init__.py
@@ -3,5 +3,5 @@ import os.path
 from .get_data import *
 from .data_prep import *
 
-_dist = importlib.metadata.metadata('libml')
+_dist = importlib.metadata.metadata('libml_URLPhishing')
 __version__ = _dist['Version']
