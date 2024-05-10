a4a8a8d575929c29db8aebc796c72110c007d525
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-05-10 18:10:17 +0200

    bump setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='libml_URLPhishing',
-    version='0.0.2',
+    version='0.0.3',
     description='Library to download and process data for the URL phishing project',
     author='Shantnav Agarwal',
     project_urls={
