2ba12edcf8c1da16f2a57d387cc3fca0bf1cf1e9
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-05-10 19:10:12 +0200

    bug fix release.yaml and pypi

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='libml_URLPhishing',
-    version='0.0.9',
+    version='0.0.10',
     description='Library to download and process data for the URL phishing project',
     author='Shantnav Agarwal',
     project_urls={
-        'Documentation': '',
+        'Documentation': 'https://remla24-team-15.github.io/libml/',
         'Source Code': 'https://github.com/REMLA24-TEAM-15/libml'
     },
 )
