6e24032fb89673e136d2875ca1b5498f43e8484e
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-05-10 18:09:10 +0200

    Added project.toml, update release.yaml and setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
-
 setup(
-    name='libml',
+    name='libml_URLPhishing',
     version='0.0.2',
-    description='Library to download and process data for the URL phishing project'
+    description='Library to download and process data for the URL phishing project',
+    author='Shantnav Agarwal',
+    project_urls={
+        'Documentation': '',
+        'Source Code': 'https://github.com/REMLA24-TEAM-15/libml'
+    },
 )
