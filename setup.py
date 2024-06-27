3df7ef4920bfa41f1f4e014b09d2d44317683f3b
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-06-27 21:34:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='libml_URLPhishing',
-    version='4.1.4',
+    version='4.1.5',
     long_description='Library to download and process data for the URL phishing project',
     description='Library to download and process data for the URL phishing project',
     author='Shantnav Agarwal',
@@ -11,8 +11,8 @@ setup(
         'Source Code': 'https://github.com/REMLA24-TEAM-15/libml'
     },
     install_requires=[
-        'tensorflow>=2.16.1',
-        'numpy>=1.26.4',
-        'scikit-learn>=1.4.0'
+        'tensorflow>=2.16',
+        'numpy>=1.26',
+        'scikit-learn>=1.4'
     ]
 )
